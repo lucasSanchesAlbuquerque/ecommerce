214a35295c8954e7bb2935586e3c22a5f2c6d399
Lucas SA <lusanchesx@gmail.com>
2025-01-07 11:25:53 -0300

    product findAll paginado

--- src/main/java/com/lucassa/ecomerce/controllers/ProductController.java
+++ src/main/java/com/lucassa/ecomerce/controllers/ProductController.java
@@ -1,16 +1,16 @@
 package com.lucassa.ecomerce.controllers;
 
 import com.lucassa.ecomerce.dto.ProductDto;
-import com.lucassa.ecomerce.entities.Product;
-import com.lucassa.ecomerce.repositories.ProductRepository;
 import com.lucassa.ecomerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,8 +21,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    public ProductDto findById(){
-        return productService.findBiId(1L);
+    public Page<ProductDto> findAll(Pageable pageable){
+        return productService.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}")
--- src/main/java/com/lucassa/ecomerce/services/ProductService.java
+++ src/main/java/com/lucassa/ecomerce/services/ProductService.java
@@ -3,21 +3,35 @@ package com.lucassa.ecomerce.services;
 import com.lucassa.ecomerce.dto.ProductDto;
 import com.lucassa.ecomerce.entities.Product;
 import com.lucassa.ecomerce.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductService {
 
     @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
     private ProductRepository productRepository;
 
     @Transactional(readOnly = true)
     public ProductDto findBiId(Long id){
         Optional<Product> product = productRepository.findById(id);
-        return new ProductDto(product.get());
+        return  new ProductDto(product.get());
+        //return modelMapper.map(product,ProductDto.class);
     }
+
+    public Page<ProductDto> findAll(Pageable pageable){
+        Page<Product> products = productRepository.findAll(pageable);
+        return products.map(x -> new ProductDto(x));
+    }
+
+
 }
